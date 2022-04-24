a6aa5de48faca4206f709b1d7bb9f07e90a67b90
vulca1n <r.chenna15@gmail.com>
2022-04-24 18:17:45 +0000

    adding other pages

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,7 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
+
     res.render('main')
 })
-
+router.get('/blog', (req, res, next) => {
+    res.render('blog')
+})
+router.get('/contact', (req, res, next) => {
+    res.render('contact')
+})
+router.get('/about', (req, res, next) => {
+    res.render('about')
+})
+router.get('/services', (req, res, next) => {
+    res.render('service')
+})
+router.get('/pricing', (req, res, next) => {
+    res.render('pricing')
+})
 module.exports = router
