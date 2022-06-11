3d252cf7dbb4a9814130e5d1d2353eb90cba7af2
vulca1n <r.chenna15@gmail.com>
2022-06-12 00:26:14 +0100

    new update

--- app.js
+++ app.js
@@ -1,21 +1,73 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const methodOverride = require('method-override')
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./src/models/userModel');
+const Routes = require('./src/routes/main');
 
-const app = express()
-const port = 5000
-    // listen  on port 5000
-const config = {
-    views: 'views',
-    static: 'public',
-    logging: true,
 
-}
 
-const main = require('./src/routes/main')
-app.use('/', main)
-app.use(express.static('public'))
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
 
-app.listen(port, () => console.log('Listneing on port' + port))
+mongoose.connect('mongodb://localhost:27017/fraud', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
 
-module.exports = app
+    })
+    .then(() => {
+        console.log("Database Connected");
+    })
+    .catch(err => {
+        console.log("DB Not Connected");
+        console.log(err);
+    })
+
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))
+
+
+app.use(session({
+    secret: 'thisisnotagoodsecret',
+    resave: false,
+    saveUninitialized: true
+}))
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// seedDB();
+
+app.get('/', (req, res) => {
+    res.render('main');
+})
+
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+})
+
+// Routes
+
+
+app.use(Routes);
+
+
+
+app.listen(5000, () => {
+    console.log("Server Running At port 5000");
+})
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const User = require('../models/userModel');
+const passport = require('passport');
+const { isLoggedIn } = require('C:/Users/utilisateur/Desktop/fraud-detection/middleware.js');
+
+
+
 
 router.get('/', (req, res, next) => {
 
@@ -14,17 +20,57 @@ router.get('/contact', (req, res, next) => {
 router.get('/about', (req, res, next) => {
     res.render('about')
 })
+router.get('/secret', isLoggedIn, async(req, res) => {
+
+    const user = await User.findById(req.user._id);
+
+    res.render('secret');
+})
+
 router.get('/service', (req, res, next) => {
     res.render('service')
 })
 router.get('/pricing', (req, res, next) => {
-    res.render('pricing')
+        res.render('pricing')
+    })
+    // get login page
+
+router.get('/login', (req, res) => {
+
+    res.render('login');
 })
-router.get('/login', (req, res, next) => {
-    res.render('login')
+
+
+// Login the existing user
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
+
+
+    res.redirect('/secret');
 })
-router.get('/register', (req, res, next) => {
-    res.render('register')
+
+
+// Logout the user from the current session
+router.get('/logout', (req, res) => {
+    req.logout();
+
+    res.redirect('/login');
+})
+
+router.get('/register', (req, res) => {
+    res.render('register');
+})
+
+// Register the new user
+router.post('/register', async(req, res) => {
+
+    const user = {
+        username: req.body.username,
+        email: req.body.email
+    }
+
+    const newUser = await User.register(user, req.body.password);
+
+    res.redirect('/login');
 })
 
 module.exports = router
